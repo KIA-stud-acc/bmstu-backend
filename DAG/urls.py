d971d57a33c37e6358cfd1f2c25b90cd81704db2
Илья <iliakekin52@gmail.com>
2023-12-17 22:35:54 +0300

    second commit

--- DAG/urls.py
+++ DAG/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from DAG_LAB import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vybory/', views.NameOptionsList.as_view()),
